20a7209f719bcae37c9b05e48ff1210b9055d7ad
Andy <andyadria7@gmail.com>
2024-10-07 12:29:38 -0100

    dist

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,12 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/VueJs-tic-tac-toe/',
+  base:isProduction ? '/VueJs-tic-tac-toe/': '/',
   plugins: [vue()],
   resolve: {
     alias: {
